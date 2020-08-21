2e694b649410b5c5c5430417456530284bf82004
rickywid <rickywid@hotmail.com>
2020-08-21 03:18:10 -0400

    changes

--- src/tests/App.spec.tsx
+++ src/tests/App.spec.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, waitFor } from '@testing-library/react';
+import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import App from '../App';
 import fetchMock from "jest-fetch-mock";
 
